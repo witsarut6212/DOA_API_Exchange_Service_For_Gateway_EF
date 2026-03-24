7472f08653de399892aaf085229d0cf45cdbf0da
witsarut <witsarut@swtech.co.th>
2026-03-24 12:20:43 +0700

    add maxlength,url only in request model api register

--- Models/Requests/ApplicationRegister.cs
+++ Models/Requests/ApplicationRegister.cs
@@ -5,13 +5,19 @@ namespace DOA_API_Exchange_Service_For_Gateway.Models.Requests;
 public class ApplicationRegisterRequest
 {
     [Required]
+    [MaxLength(400)]
     public string AppName { get; set; } = null!;
 
     [Required]
+    [MaxLength(400)]
     public string AppNickName { get; set; } = null!;
 
     [Required]
+    [MaxLength(400)]
+    [Url]
     public string HostUrl { get; set; } = null!;
 
+    [MaxLength(400)]
+    [Url]
     public string? CallbackUrl { get; set; }
 }
