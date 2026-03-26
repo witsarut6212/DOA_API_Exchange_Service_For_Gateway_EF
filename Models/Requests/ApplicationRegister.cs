19595ed315dc04372b04e4fc5af576acf076d63f
witsarut <witsarut@swtech.co.th>
2026-03-26 18:09:35 +0700

    camel case  request model update API register

--- Models/Requests/ApplicationRegister.cs
+++ Models/Requests/ApplicationRegister.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
 
 namespace DOA_API_Exchange_Service_For_Gateway.Models.Requests;
 
@@ -6,18 +7,22 @@ public class ApplicationRegisterRequest
 {
     [Required]
     [MaxLength(400)]
+    [JsonProperty("appName")]
     public string AppName { get; set; } = null!;
 
     [Required]
     [MaxLength(400)]
+    [JsonProperty("appNickName")]
     public string AppNickName { get; set; } = null!;
 
     [Required]
     [MaxLength(400)]
     [Url]
+    [JsonProperty("hostUrl")]
     public string HostUrl { get; set; } = null!;
 
     [MaxLength(400)]
     [Url]
+    [JsonProperty("callbackUrl")]
     public string? CallbackUrl { get; set; }
 }
